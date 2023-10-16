740f29c5797cb16f7047a8bc41fe953a41644d49
rafaella-nunes <faellanunes01@gmail.com>
2023-10-15 21:50:29 -0300

    feat: add image form component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Logo/>
-      {/*<Logo />
       <ImageLinkForm/>
-  <FaceRecognition/>*/}
+      {/*<FaceRecognition/>*/}
     </div>
   );
 }
