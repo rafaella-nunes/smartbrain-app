fb70ff4b7003bb5763c5094f77cee35f96dd13dc
rafaella-nunes <faellanunes01@gmail.com>
2023-10-18 14:14:39 -0300

    refactor: changes function to class

--- src/App.js
+++ src/App.js
@@ -1,109 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { useCallback } from "react";
-import Particles from 'react-particles';
-import { loadFull } from "tsparticles";
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
-function App() {
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
-    await loadFull(engine);
-}, []);
-
-const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-}, []);
-
-
-  return (
-    <div className="App">
-<Particles className='particles'
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push"
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse"
-              },
-              resize: true
-            },
-            modes: {
-              push: {
-                quantity: 4
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4
-              }
-            }
-          },
-          particles: {
-            color: {
-              value: "#ffffff"
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1
-            },
-            collisions: {
-              enable: true
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce"
-              },
-              random: false,
-              speed: 1,
-              straight: false
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800
-              },
-              value: 80
-            },
-            opacity: {
-              value: 0.5
-            },
-            shape: {
-              type: "circle"
-            },
-            size: {
-              value: { min: 1, max: 5 }
-            }
-          },
-          detectRetina: true
-        }}
-      />
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-      {/*<FaceRecognition/>*/}
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" color="#ffffff" bg={true} />
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm/>
+        {/*<FaceRecognition/>*/}
+      </div>
+    );
+  }
 }
 
 export default App;
