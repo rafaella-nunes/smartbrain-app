14632d9a053a8ca01c2b1fa7665ad588a0445b60
rafaella-nunes <faellanunes01@gmail.com>
2023-10-28 16:06:57 -0300

    chore: add api dependecies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clarifai from "clarifai";
 import ParticlesBg from 'particles-bg'
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+
+/*const app = new Clarifai.App({
+  apiKey: "5be6e6ec2f4a4b57a49a9ca6998da44a"
+});*/
+
 
 class App extends Component{
   constructor(){
@@ -20,6 +26,17 @@ class App extends Component{
 
   onButtonSubmit = () => {
     console.log('click');
+    /*app.models
+    .predict("5be6e6ec2f4a4b57a49a9ca6998da44a",
+    "https://samples.clarifai.com/face-det.jpg")
+      .then(
+      function(response) {
+        console.log(response);
+      },
+      function(err){
+        console.log(err)
+      }
+    );*/
   }
 
   render(){
